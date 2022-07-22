16125e048cce38bd882039e42428aa9d1ab04484
uvanov <kirill.ivanov000@gmail.com>
2022-07-22 11:52:32 +0300

    Base Figure class

---
+++ src/models/figures/Figure.ts
@@ -0,0 +1,38 @@
+import { Colors } from '../Colors';
+import { Cell } from '../Cell';
+import logo from '../../images/black-bishop.png';
+
+export enum FigureNames {
+  FIGURE = 'Фигура',
+  KING = 'Король',
+  KNIGHT = 'Конь',
+  PAWN = 'Пешка',
+  QUEEN = 'Ферзь',
+  ROOK = 'Ладья',
+  BISHOP = 'Слон',
+}
+
+export class Figure {
+  color: Colors;
+  image: typeof logo | null;
+  cell: Cell;
+  name: FigureNames;
+  id: number;
+
+  constructor(color: Colors, cell: Cell) {
+    this.id = Math.random();
+    this.color = color;
+    this.cell = cell;
+    this.cell.figure = this;
+    this.image = null;
+    this.name = FigureNames.FIGURE;
+  }
+
+  CanMove(target: Cell): boolean {
+    return true;
+  }
+
+  MoveFigure(target: Cell) {
+
+  }
+}
