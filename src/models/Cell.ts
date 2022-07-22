f1991ec7ec7d4e18174eab48e7404cbae8377a65
uvanov <kirill.ivanov000@gmail.com>
2022-07-22 11:08:37 +0300

    Cell model

---
+++ src/models/Cell.ts
@@ -0,0 +1,23 @@
+import { Colors } from './Colors';
+import { Figure } from './figures/Figure';
+import { Board } from './Board';
+
+export class Cell {
+  readonly x: number;
+  readonly y: number;
+  readonly color: Colors;
+  figure: Figure | null;
+  board: Board;
+  available: boolean;
+  id: number;
+
+  constructor(board: Board, x: number, y: number, color: Colors, figure: Figure | null) {
+    this.x = x;
+    this.y = y;
+    this.board = board;
+    this.color = color;
+    this.figure = figure;
+    this.id = Math.random();
+    this.available = false;
+  }
+}
