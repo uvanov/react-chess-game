7e63645668f9cd684fca8e2cd1e38e051d6fa8bf
uvanov <kirill.ivanov000@gmail.com>
2022-07-24 15:53:52 +0300

    Pawn blocking case

--- src/models/figures/Pawn.ts
+++ src/models/figures/Pawn.ts
@@ -17,14 +17,16 @@ export class Pawn extends Figure {
   CanMove(target: Cell): boolean {
     if (!super.CanMove(target)) {
       return false;
-    };
+    }
 
     const direction = this.cell.figure?.color === Colors.BLACK ? -1 : 1;
     const firstStepDirection = this.cell.figure?.color === Colors.BLACK ? -2 : 2;
 
     const isOnDirection = target.y === this.cell.y + direction || ( this.IsFirstStep && ( target.y === this.cell.y + firstStepDirection ) );
+    const isForwardBlocked = !this.cell.board.GetCell(this.cell.x, this.cell.y + direction).IsEmpty();
 
-    if (isOnDirection && target.x === this.cell.x && target.IsEmpty()) {
+
+    if (!isForwardBlocked && isOnDirection && target.x === this.cell.x && target.IsEmpty()) {
       return true;
     }
 
